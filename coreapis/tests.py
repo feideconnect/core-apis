d8f5ed6dda43b1b5a16dd3f8dab4f3e60b16df24
Sigmund Augdal <sigmund.augdal@uninett.no>
2014-12-15 11:21:16 +0100

    Replace scaffold webapp with three simple testing endpoints

--- coreapis/tests.py
+++ coreapis/tests.py
@@ -6,12 +6,25 @@ from pyramid import testing
 class ViewTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
+        self.config.include('coreapis.views.configure')
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
+    def test_test_open(self):
+        from .views import test_open
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'core-apis')
+        info = test_open(request)
+        self.assertEqual(info['status'], 'open')
+
+    def test_test_client_unauthorized(self):
+        from .views import test_client
+        request = testing.DummyRequest()
+        info = test_client(request)
+        self.assertEqual(request.response.status_code, 402)
+
+    def test_test_user_unauthorized(self):
+        from .views import test_user
+        request = testing.DummyRequest()
+        info = test_user(request)
+        self.assertEqual(request.response.status_code, 402)
--- coreapis/views.py
+++ coreapis/views.py
@@ -1,6 +1,22 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'core-apis'}
+def configure(config):
+    config.add_route('test_open', '/open')
+    config.add_route('test_client', '/client')
+    config.add_route('test_user', '/user')
+
+
+@view_config(route_name='test_open', renderer='json')
+def test_open(request):
+    return {'status': 'open'}
+
+
+@view_config(route_name='test_client', renderer='json')
+def test_client(request):
+    return {'status': 'open'}
+
+
+@view_config(route_name='test_user', renderer='json')
+def test_user(request):
+    return {'status': 'open'}
