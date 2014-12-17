8a8e9e682828a0b1eae310cd1c9329d8dcc766e7
Sigmund Augdal <sigmund.augdal@uninett.no>
2014-12-17 12:33:01 +0100

    Added unit tests for tokens with bad clientid, scope, validuntil and with no userid

--- coreapis/tests.py
+++ coreapis/tests.py
@@ -44,7 +44,7 @@ class TokenValidationTests(unittest.TestCase):
             'clientid': uuid.uuid4(),
             'userid': uuid.uuid4(),
             'access_token': uuid.uuid4(),
-            'validuntil': datetime.datetime.now(),
+            'validuntil': datetime.datetime.now() + datetime.timedelta(days=5),
             'scope': ['foo'],
         }
 
@@ -52,9 +52,30 @@ class TokenValidationTests(unittest.TestCase):
         pass
 
     def test_token_valid(self):
-        self.token['validuntil'] += datetime.timedelta(days=10)
         assert self.middleware.token_is_valid(self.token, self.token['access_token']) is True
 
     def test_expired_token(self):
         self.token['validuntil'] -= datetime.timedelta(days=10)
         assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+
+    def test_bad_client(self):
+        self.token['clientid'] = None
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+        del self.token['clientid']
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+
+    def test_bad_scope(self):
+        self.token['scope'] = None
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+        del self.token['scope']
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+
+    def test_bad_validuntil(self):
+        self.token['validuntil'] = None
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+        del self.token['validuntil']
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is False
+
+    def test_no_userid(self):
+        self.token['userid'] = None
+        assert self.middleware.token_is_valid(self.token, self.token['access_token']) is True
