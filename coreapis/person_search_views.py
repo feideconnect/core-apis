e3b287f54b02af76d91ec934a56df27b04862880
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-01-05 16:10:16 +0100

    Read ldap config from config file supporting multiple servers per org

--- coreapis/person_search_views.py
+++ coreapis/person_search_views.py
@@ -2,22 +2,37 @@ from pyramid.view import view_config
 from pyramid.exceptions import HTTPNotFound
 import logging
 import ldap3
+import json
 
 
 def configure(config):
     config.add_route('person_search', '/search/{org}/{name}')
 
 
+def get_ldap_config():
+    return json.load(open('ldap-config.json'))
+
+
 def get_connection(org):
-    server = ldap3.Server('ldap.uninett.no', use_ssl=True)
-    con = ldap3.Connection(server, auto_bind=True,
+    conf = get_ldap_config()
+    orgconf = conf[org]
+    server_pool = ldap3.ServerPool(None, ldap3.POOLING_STRATEGY_ROUND_ROBIN, active=True)
+    for server in orgconf['servers']:
+        if ':' in server:
+            host, port = server.split(':', 1)
+            port = int(port)
+        else:
+            host, port = server, None
+        server = ldap3.Server(host, port=port, use_ssl=True)
+        server_pool.add(server)
+    con = ldap3.Connection(server_pool, auto_bind=True,
                            client_strategy=ldap3.STRATEGY_SYNC,
                            check_names=True)
     return con
 
 
 def get_base_dn(org):
-    return 'cn=people,dc=uninett,dc=no'
+    return get_ldap_config()[org]['base_dn']
 
 
 @view_config(route_name='person_search', renderer='json', permission='scope_personsearch')
@@ -26,7 +41,7 @@ def person_search(request):
     search = request.matchdict['name']
     if not org or not search:
         raise HTTPNotFound('missing org or search term')
-    if org != 'uninett':
+    if org not in get_ldap_config().keys():
         raise HTTPNotFound('Unknown org')
     con = get_connection(org)
     base_dn = get_base_dn(org)
