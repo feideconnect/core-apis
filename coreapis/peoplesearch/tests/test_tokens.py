e5166bba996812eeaae8b6bc8929f0e813eef9e9
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-01-12 14:48:01 +0100

    Use weak but deterministic crytography for profile image tokens, to not void caches everywhere

--- coreapis/peoplesearch/tests/test_tokens.py
+++ coreapis/peoplesearch/tests/test_tokens.py
@@ -11,10 +11,3 @@ class TestTokens(unittest.TestCase):
         assert token != msg
         msg2 = tokens.decrypt_token(token, key)
         assert msg2 == msg
-
-    def test_unpredictable(self):
-        key = os.urandom(32)
-        msg = 'My secret'
-        token = tokens.crypt_token(msg, key)
-        token2 = tokens.crypt_token(msg, key)
-        assert token != token2
--- coreapis/peoplesearch/tokens.py
+++ coreapis/peoplesearch/tokens.py
@@ -1,26 +1,22 @@
 import base64
-import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 
 def crypt_token(uuid, key):
     backend = default_backend()
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
     encryptor = cipher.encryptor()
     msg = uuid.encode('UTF-8')
     msg = msg + b'\t' * (16 - len(msg) % 16)
-    ct = base64.urlsafe_b64encode(iv + encryptor.update(msg) + encryptor.finalize()).decode('ASCII')
+    ct = base64.urlsafe_b64encode(encryptor.update(msg) + encryptor.finalize()).decode('ASCII')
     return ct
 
 
 def decrypt_token(token, key):
     token = base64.urlsafe_b64decode(token)
-    iv = token[:16]
-    data = token[16:]
     backend = default_backend()
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
     decryptor = cipher.decryptor()
-    msg = decryptor.update(data) + decryptor.finalize()
+    msg = decryptor.update(token) + decryptor.finalize()
     return msg.decode('UTF-8').strip()
