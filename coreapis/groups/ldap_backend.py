8202ef0a0eb22bbf5aa00d4623d80029ba3393ec
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-02-05 11:30:44 +0100

    First shot at an ldap backend for group engine. Currently only supports listing org and orgunits of the current user

---
+++ coreapis/groups/ldap_backend.py
@@ -0,0 +1,90 @@
+from coreapis.utils import LogWrapper
+from . import BaseBackend
+import eventlet
+ldap3 = eventlet.import_patched('ldap3')
+from coreapis.peoplesearch.controller import LDAPController
+from eventlet.pools import Pool
+
+ldap_type = 'voot:ldap_feide'
+
+
+class LDAPBackend(BaseBackend):
+    def __init__(self, prefix, maxrows, config):
+        super(LDAPBackend, self).__init__(prefix, maxrows, config)
+        self.log = LogWrapper('groups.ldapbackend')
+        timer = config.get_settings().get('timer')
+        self.ldap = LDAPController(timer, pool=Pool)
+
+    def get_member_groups(self, user, show_all):
+        result = []
+        feideid = None
+        for sec in user['userid_sec']:
+            if sec.startswith('feide:'):
+                feideid = sec.split(':', 1)[1]
+        if not feideid:
+            raise RuntimeError('could not find feide id')
+        if not '@' in feideid:
+            raise RuntimeError('invalid feide id')
+        realm = feideid.split('@', 1)[1]
+        base_dn = self.ldap.get_base_dn(realm)
+        res = self.ldap.search(realm, base_dn, '(eduPersonPrincipalName={})'.format(feideid),
+                               ldap3.SEARCH_SCOPE_WHOLE_SUBTREE,
+                               ('eduPersonOrgDN', 'eduPersonOrgUnitDN'), 1)
+        if len(res) == 0:
+            raise KeyError('could not find user in catalog')
+        res = res[0]
+        attributes = res['attributes']
+        if 'eduPersonOrgDN' in attributes:
+            orgDN = attributes['eduPersonOrgDN'][0]
+            print("Got orgDN: {}".format(orgDN))
+            org = self.ldap.search(realm, orgDN, '(objectClass=*)'.format(feideid),
+                                   ldap3.SEARCH_SCOPE_BASE_OBJECT,
+                                   ldap3.ALL_ATTRIBUTES, 1)
+            if len(org) == 0:
+                raise KeyError('orgDN not found in catalog')
+            org = org[0]
+            orgAttributes = org['attributes']
+            # for key, value in orgAttributes.items():
+            #     print('{}: {}'.format(key, value))
+            result.append({
+                'id': '{}:{}'.format(self.prefix, realm),
+                'displayName': orgAttributes['cn'][0],
+                'type': 'eduperson:org',
+            })
+        if 'eduPersonOrgUnitDN' in attributes:
+            orgUnitDN = attributes['eduPersonOrgUnitDN'][0]
+            print("Got orgUnitDN: {}".format(orgUnitDN))
+            ou = self.ldap.search(realm, orgUnitDN, '(objectClass=*)'.format(feideid),
+                                  ldap3.SEARCH_SCOPE_BASE_OBJECT,
+                                  ldap3.ALL_ATTRIBUTES, 1)
+            if len(ou) == 0:
+                raise KeyError('orgUnitDN not found in catalog')
+            ou = ou[0]
+            ouAttributes = ou['attributes']
+            # for key, value in ouAttributes.items():
+            #     print('{}: {}'.format(key, value))
+            result.append({
+                'id': '{}:{}:{}'.format(self.prefix, realm,
+                                        ouAttributes['norEduOrgUnitUniqueIdentifier'][0]),
+                'displayName': ouAttributes['ou'][0],
+                'type': 'eduperson:orgunit',
+            })
+        return result
+
+    def grouptypes(self):
+        return [
+            {
+                "id": "eduperson:orgunit",
+                "displayName": {
+                    "en": "Organizational unit",
+                    "nb": "Organisasjonsenhet"
+                }
+            },
+            {
+                "id": "eduperson:org",
+                "displayName": {
+                    "en": "Organization",
+                    "nb": "Organisasjon"
+                }
+            },
+        ]
