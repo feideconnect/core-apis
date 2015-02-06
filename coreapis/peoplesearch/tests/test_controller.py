39b564460e830ccba35bf48ed19ce0479e0cd727
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-02-06 17:37:09 +0100

    Fix ldap controller unit tests by mocking open for the the ldap-config.json

--- coreapis/peoplesearch/tests/test_controller.py
+++ coreapis/peoplesearch/tests/test_controller.py
@@ -110,7 +110,9 @@ class TestProfileImageFetch(TestCase):
 
 class TestLookupFeideid(TestCase):
     def setUp(self):
-        self.ldap = controller.LDAPController(mock.MagicMock())
+        m = mock.mock_open(read_data='{}')
+        with mock.patch('coreapis.peoplesearch.controller.open', m, create=True):
+            self.ldap = controller.LDAPController(mock.MagicMock())
 
     def test_feide_multiple_users(self):
         self.ldap.ldap_search = mock.MagicMock(return_value=[{'attributes': {'cn': ['Test User']}},
