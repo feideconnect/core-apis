5db9918c01d92c7173600a0fcbff3baf0970e244
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-02-26 18:08:27 +0100

    Pretty print json output by default

--- coreapis/__init__.py
+++ coreapis/__init__.py
@@ -55,7 +55,7 @@ def main(global_config, **settings):
     config.scan('coreapis.error_views')
     config.add_settings(realm=global_config['oauth_realm'])
     config.add_tween('coreapis.utils.RequestTimingTween')
-    json_renderer = pyramid.renderers.JSON()
+    json_renderer = pyramid.renderers.JSON(indent=4)
     json_renderer.add_adapter(datetime.datetime, lambda x, y: format_datetime(x))
     json_renderer.add_adapter(blist.sortedset, lambda x, y: list(x))
     json_renderer.add_adapter(uuid.UUID, lambda x, y: str(x))
