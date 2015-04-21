0096facecfbbd9e8c2c799a80ad9d3460f3db135
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-04-21 14:57:12 +0200

    Include organization in properties returned for clients by default

--- coreapis/cassandra_client.py
+++ coreapis/cassandra_client.py
@@ -39,7 +39,7 @@ class Client(object):
         )
         self.prepared = {}
         self.default_columns = {
-            'clients': 'owner,name,type,status,scopes_requested,client_secret,created,redirect_uri,descr,id,scopes,updated',
+            'clients': 'owner,name,type,status,scopes_requested,client_secret,created,redirect_uri,descr,id,scopes,updated,organization',
             'apigk': 'id,requireuser,created,name,scopedef,httpscertpinned,status,descr,expose,updated,trust,endpoints,owner',
             'groups': 'id,created,descr,name,owner,public,updated,invitation_token',
             'group_members': 'userid,groupid,status,type',
