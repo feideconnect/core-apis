1b7e9b93f546f1311d2131e2c114a324f1d98491
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-05-27 12:54:23 +0200

    Move cache class into separate file

--- coreapis/groups/fs_backend.py
+++ coreapis/groups/fs_backend.py
@@ -1,35 +1,10 @@
-import time
 import functools
 from coreapis.utils import LogWrapper, get_feideids, failsafe, translatable
+from coreapis.cache import Cache
 from coreapis import cassandra_client
 from . import BaseBackend
 from eventlet.greenpool import GreenPool
 import requests
-
-
-class Cache(object):
-    def __init__(self, expiry):
-        self.log = LogWrapper('groups.fs_backend.cache')
-        self.data = dict()
-        self.expiry = expiry
-
-    def _set(self, key, value):
-        self.data[key] = (time.time(), value)
-        return value
-
-    def _update(self, key, getter):
-        return self._set(key, getter())
-
-    def get(self, key, getter):
-        if not key in self.data:
-            self.log.debug('cache miss', key=key)
-            return self._update(key, getter)
-        ts, cached = self.data.get(key)
-        if ts + self.expiry < time.time():
-            self.log.debug('cache expired', key=key)
-            return self._update(key, getter)
-        self.log.debug('cache hit', key=key)
-        return cached
 
 
 class FsBackend(BaseBackend):
