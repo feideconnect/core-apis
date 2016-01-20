6091c5ead0cd7cb893d3d2de29fd03bdabf22347
Jon K Hellan <jon.kare.hellan@uninett.no>
2016-01-20 11:13:01 +0100

    Fix adhocgroupadm.list that broke with driver upgrade
    We appended to the result set. Result set must now be converted to list
    first.

--- coreapis/adhocgroupadm/controller.py
+++ coreapis/adhocgroupadm/controller.py
@@ -78,7 +78,7 @@ class AdHocGroupAdmController(CrudControllerBase):
         self.session.delete_group(groupid)
 
     def list(self, userid, params):
-        groups = self.session.get_groups(['owner = ?'], [userid], self.maxrows)
+        groups = list(self.session.get_groups(['owner = ?'], [userid], self.maxrows))
         seen_groups = set([group['id'] for group in groups])
         memberships = self.session.get_group_memberships(userid, "admin", "normal", self.maxrows)
         for mem in memberships:
