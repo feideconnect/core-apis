e94c5ae8364df591a5003550e40e0b859c87ca9a
Jon K Hellan <jon.kare.hellan@uninett.no>
2016-02-02 15:18:52 +0100

    Do not accumulate pylint result from tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,10 +5,8 @@ pip install pylint
 pip install coverage
 pip install --upgrade setuptools
 python setup.py develop
-pylint -f parseable coreapis >pylint.out || result=$?
+pylint -f parseable coreapis >pylint.out
 echo "pylint returned $result"
 coverage run --branch -m py.test --junitxml=testresults.xml || true
-res=$(($result&35))
 coverage html --include 'coreapis/*'
 coverage xml --include 'coreapis/*'
-exit 0
