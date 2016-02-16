1221917443be078607fffd4676af4727c1f08b63
Sigmund Augdal <sigmund.augdal@uninett.no>
2016-02-16 16:04:11 +0100

    Change name of docker repository from feideconnect to dataporten

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -e
-base_name=uninett-docker-uninett.bintray.io/feideconnect/core-apis
+base_name=uninett-docker-uninett.bintray.io/dataporten/core-apis
 docker build -t ${base_name}-base:$GIT_COMMIT .
 docker tag -f ${base_name}-base:$GIT_COMMIT ${base_name}-base:latest
 base_dir=$(pwd)
