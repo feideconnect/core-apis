28d756d09b5a1663ac8ea9816a27808df882df93
Sigmund Augdal <sigmund.augdal@uninett.no>
2016-08-11 15:48:42 +0200

    Fix pushing of -base image

--- push-docker.sh
+++ push-docker.sh
@@ -1,7 +1,6 @@
 base_name=uninett-docker-uninett.bintray.io/dataporten/core-apis
 
-docker push "${base_name}"
-for app in api-gatekeeper core-apis groupengine
+for app in base api-gatekeeper core-apis groupengine
 do
     image="${base_name}-${app}"
     docker push "${image}"
