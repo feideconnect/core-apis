771345800d8baf84b6088ff08e025e5e27ba78d3
Jon K Hellan <jon.kare.hellan@uninett.no>
2016-10-28 14:56:58 +0200

    Do not make admin_contact a public attr

--- coreapis/clientadm/controller.py
+++ coreapis/clientadm/controller.py
@@ -83,8 +83,7 @@ class ClientAdmController(CrudControllerBase):
         'admin_contact': V.Nullable('string', None),
     }
     public_attrs = ['id', 'name', 'descr', 'redirect_uri', 'owner', 'organization', 'authproviders',
-                    'systemdescr', 'privacypolicyurl', 'homepageurl', 'loginurl', 'supporturl',
-                    'admin_contact']
+                    'systemdescr', 'privacypolicyurl', 'homepageurl', 'loginurl', 'supporturl']
     scope_attrs = ['scopes', 'scopes_requested']
     platformadmin_attrs = ['owner', 'scopes', 'orgauthorization']
     platformadmin_attrs_update = ['organization']
