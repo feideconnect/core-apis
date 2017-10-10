8bf82ec27ac9d7b605daaeca316924141e486cbf
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-10-10 10:11:20 +0200

    Fix incorrect coverage reports for eventlet code

--- run-tests.sh
+++ run-tests.sh
@@ -17,6 +17,6 @@ docker-compose run coreapis sh -c "pylint -f parseable coreapis >pylint.out || t
 
 docker-compose run dataportenschemas
 
-docker-compose run coreapis sh -c "coverage run --branch -m py.test --junitxml=testresults.xml || true"
+docker-compose run coreapis sh -c "coverage run --concurrency eventlet --branch -m py.test --junitxml=testresults.xml || true"
 docker-compose run coreapis sh -c "coverage html --include 'coreapis/*'"
 docker-compose run coreapis sh -c "coverage xml --include 'coreapis/*'"
