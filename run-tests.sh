b0abbbb508f787dc2a789a39ff841e9cdfe61588
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-12-07 09:50:40 +0100

    Various tweaks to allow faster manual test runs by setting NO_CLEANUP and NO_PYLINT variables

--- run-tests.sh
+++ run-tests.sh
@@ -2,10 +2,20 @@
 set -e
 export COMPOSE_FILE=compose-test-cassandra.yml
 
+up_services=$(docker-compose ps|grep -c Up||true)
+if [ $up_services -eq 3 ]
+then
+    do_setup=0
+else
+    do_setup=1
+fi
+
 # Set up cassandra test environment
-docker-compose pull
-docker-compose up -d cassandra
-docker-compose up -d ldap
+if [ $do_setup -eq 1 ]
+then
+    docker-compose pull
+    docker-compose up -d
+fi
 function clean-docker() {
     docker-compose kill
     docker-compose rm --force -v
@@ -15,10 +25,15 @@ then
     trap clean-docker EXIT
 fi
 
-docker-compose build coreapis
-docker-compose run -u ${UID}:${GID} coreapis sh -c "pylint -f parseable coreapis >pylint.out || true"
+if [ -z "${NO_PYLINT}" ]
+then
+    docker-compose exec -u ${UID}:${GID} coreapis sh -c "pylint -f parseable coreapis >pylint.out || true"
+fi
 
-docker-compose run dataportenschemas
+if [ $do_setup -eq 1 ]
+then
+    docker-compose run dataportenschemas
+fi
 
-docker-compose run -u ${UID}:${GID} coreapis sh -c "py.test -m 'not eventlet' --cov --cov-report=html --cov-report=xml --junitxml=testresults.xml $@||true"
-docker-compose run -u ${UID}:${GID} coreapis sh -c "py.test -m eventlet --cov --cov-append --cov-report=html --cov-report=xml --junitxml=testresults-eventlet.xml $@||true"
+docker-compose exec -u ${UID}:${GID} coreapis sh -c "py.test -m 'not eventlet' --cov --cov-report=html --cov-report=xml --junitxml=testresults.xml $@||true"
+docker-compose exec -u ${UID}:${GID} coreapis sh -c "py.test -m eventlet --cov --cov-append --cov-report=html --cov-report=xml --junitxml=testresults-eventlet.xml $@||true"
