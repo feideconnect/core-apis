54f5152b3bff369712a1f02de9672e1a8535ea16
Jon K Hellan <jon.kare.hellan@uninett.no>
2018-03-18 22:46:57 +0100

    Refactor ldap status

--- coreapis/ldap/status.py
+++ coreapis/ldap/status.py
@@ -25,6 +25,88 @@ def check_object(con, dn, objtype, needed_attributes):
     return errors
 
 
+def check_attributes(connection):
+    attributes = connection.response[0]['attributes']
+    errors = []
+    for attribute in coreapis.ldap.REQUIRED_PERSON_ATTRIBUTES:
+        if attribute not in attributes:
+            errors.append('Attribute {} missing on person'.format(attribute))
+    if 'eduPersonOrgDN' in attributes:
+        orgDN = coreapis.ldap.get_single(attributes['eduPersonOrgDN'])
+        errors.extend(check_object(connection, orgDN, 'organization',
+                                   coreapis.ldap.REQUIRED_ORG_ATTRIBUTES))
+    if 'eduPersonOrgUnitDN' in attributes:
+        orgUnitDN = attributes['eduPersonOrgUnitDN'][0]
+        errors.extend(check_object(connection, orgUnitDN, 'organizational unit',
+                                   coreapis.ldap.REQUIRED_ORG_UNIT_ATTRIBUTES))
+    return errors
+
+
+def get_search_status(connection):
+    if len(connection.response) == 0:
+        return {
+            'result': 'empty response looking up feideid',
+        }
+    errors = check_attributes(connection)
+    if errors:
+        return {
+            'result': 'Data Error',
+            'message': "\n".join(errors),
+        }
+    else:
+        return {
+            'result': 'success',
+        }
+
+
+def record_ldap_exception(ex, result, message):
+    status = {
+        'result': result,
+        'class': ex.__class__.__name__,
+        'message': message,
+    }
+    if len(ex.args) > 1 and isinstance(ex.args[1], list) and len(ex.args[1][0]) > 2:
+        status['details'] = ex.args[1][0][2].args[0]
+    return status
+
+
+def record_exception(ex):
+    message = 'Unknown error'
+    if len(ex.args) > 0:
+        message = ex.args[0]
+    status = {
+        'result': 'other error',
+        'class': ex.__class__.__name__,
+        'message': message,
+    }
+    return status
+
+
+def server_status(server, base_dn, search_filter, user, password, tls):
+    if ':' in server:
+        host, port = server.split(':', 1)
+        port = int(port)
+    else:
+        host, port = server, None
+    ldapserver = ldap3.Server(host, port=port, use_ssl=True, connect_timeout=1, tls=tls)
+
+    try:
+        con = ldap3.Connection(ldapserver, auto_bind=True,
+                               user=user, password=password,
+                               client_strategy=ldap3.SYNC,
+                               return_empty_attributes=False,
+                               check_names=True)
+        con.search(base_dn, search_filter, ldap3.SUBTREE,
+                   attributes=coreapis.ldap.REQUIRED_PERSON_ATTRIBUTES, size_limit=1)
+        status = get_search_status(con)
+    except ldap3.core.exceptions.LDAPCommunicationError as ex:
+        status = record_ldap_exception(ex, 'Communications Error', ldap_exception_argument(ex))
+    except ldap3.core.exceptions.LDAPBindError as ex:
+        status = record_ldap_exception(ex, 'bind_error', ex.args[0])
+    except Exception as ex:
+        status = record_exception(ex)
+    return status
+
 def ldap_status(realm, feideid, ldap_config, ldap_certs):
 
     if not realm or realm not in ldap_config:
@@ -43,72 +125,5 @@ def ldap_status(realm, feideid, ldap_config, ldap_certs):
         user = None
         password = None
     for server in orgconfig['servers']:
-        if ':' in server:
-            host, port = server.split(':', 1)
-            port = int(port)
-        else:
-            host, port = server, None
-        ldapserver = ldap3.Server(host, port=port, use_ssl=True, connect_timeout=1, tls=tls)
-
-        try:
-            con = ldap3.Connection(ldapserver, auto_bind=True,
-                                   user=user, password=password,
-                                   client_strategy=ldap3.SYNC,
-                                   return_empty_attributes=False,
-                                   check_names=True)
-            con.search(base_dn, search_filter, ldap3.SUBTREE,
-                       attributes=coreapis.ldap.REQUIRED_PERSON_ATTRIBUTES, size_limit=1)
-            if len(con.response) == 0:
-                status[server] = {
-                    'result': 'empty response looking up feideid',
-                }
-                continue
-            errors = []
-            attributes = con.response[0]['attributes']
-            for attribute in coreapis.ldap.REQUIRED_PERSON_ATTRIBUTES:
-                if attribute not in attributes:
-                    errors.append('Attribute {} missing on person'.format(attribute))
-            if 'eduPersonOrgDN' in attributes:
-                orgDN = coreapis.ldap.get_single(attributes['eduPersonOrgDN'])
-                errors.extend(check_object(con, orgDN, 'organization',
-                                           coreapis.ldap.REQUIRED_ORG_ATTRIBUTES))
-            if 'eduPersonOrgUnitDN' in attributes:
-                orgUnitDN = attributes['eduPersonOrgUnitDN'][0]
-                errors.extend(check_object(con, orgUnitDN, 'organizational unit',
-                                           coreapis.ldap.REQUIRED_ORG_UNIT_ATTRIBUTES))
-            if errors:
-                status[server] = {
-                    'result': 'Data Error',
-                    'message': "\n".join(errors),
-                }
-            else:
-                status[server] = {
-                    'result': 'success',
-                }
-        except ldap3.core.exceptions.LDAPCommunicationError as ex:
-            status[server] = {
-                'result': 'Communications Error',
-                'class': ex.__class__.__name__,
-                'message': ldap_exception_argument(ex),
-            }
-            if len(ex.args) > 1 and isinstance(ex.args[1], list) and len(ex.args[1][0]) > 2:
-                status[server]['details'] = ex.args[1][0][2].args[0]
-        except ldap3.core.exceptions.LDAPBindError as ex:
-            status[server] = {
-                'result': 'bind_error',
-                'class': ex.__class__.__name__,
-                'message': ex.args[0],
-            }
-            if len(ex.args) > 1 and isinstance(ex.args[1], list) and len(ex.args[1][0]) > 2:
-                status[server]['details'] = ex.args[1][0][2].args[0]
-        except Exception as ex:
-            message = 'Unknown error'
-            if len(ex.args) > 0:
-                message = ex.args[0]
-            status[server] = {
-                'result': 'other error',
-                'class': ex.__class__.__name__,
-                'message': message,
-            }
-
+        status[server] = server_status(server, base_dn, search_filter, user, password, tls)
     return status
